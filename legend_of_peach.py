e0d802a308dcbf706de0f0d88f13f56d22f9307d
Randy Le <97Randy.le@gmail.com>
2019-10-11 13:54:33 -0700

    adding old files

---
+++ legend_of_peach.py
@@ -0,0 +1,30 @@
+import pygame
+from hub import Hub
+
+
+def run_game():
+    """ Main game structure that runs the whole program """
+    # Initialize pygame
+    pygame.init()
+
+    # Set up the hub
+    hub = Hub()
+    pygame.display.set_caption(hub.WINDOW_TITLE)
+    pygame.display.set_icon(hub.WINDOW_ICON)
+
+    while True:
+        """ Game Loop, as long as this is true the game will run. """
+        # Clear Screen
+        hub.main_screen.fill(hub.BG_COLOR)
+
+        # Decide what screen to display
+        hub.display_screen()
+
+        # Display the screen onto the window
+        pygame.display.flip()
+        hub.CLOCK.tick(hub.FRATERATE)
+
+
+# If the console command runs this file, it will run the game
+if __name__ == '__main__':
+    run_game()
